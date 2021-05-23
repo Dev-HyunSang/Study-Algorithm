c823da3575904c19cbfb48efee5020c6133d638f
HyunSang Park <hyun.sang@parkhyunsang.com>
2021-05-23 16:32:36 +0900

    [UPDATE🚀]  10172번 - 개 / 29506954

---
+++ BAEKJOON/10172.go
@@ -0,0 +1,12 @@
+// 10172번 - 개 / 29506954
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("|\\_/|")
+	fmt.Println("|q p|   /}")
+	fmt.Println("( 0 )\"\"\"\\")
+	fmt.Println("|\"^\"`    |")
+	fmt.Println("||_/=\\\\__|")
+}
